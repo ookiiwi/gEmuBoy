b3be01478e4661a09ba8dddfd3c44bed0a126613
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-09-12 22:59:15 +0200

    Rename cputypes

---
+++ include/gbtypes.h
@@ -0,0 +1,10 @@
+#ifndef CPUTYPE_H_
+#define CPUTYPE_H_
+
+typedef unsigned char  BYTE;
+typedef signed   char  SIGNED_BYTE;
+typedef unsigned short WORD;
+typedef signed   short SIGNED_WORD;
+typedef unsigned int   DWORD;
+
+#endif
