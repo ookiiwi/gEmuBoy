a11fd398d430f67e40653cb08da30679245a3859
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-09-13 14:56:45 +0200

    Check read to boot rom if mapped

--- src/mmu.c
+++ src/mmu.c
@@ -1,17 +1,22 @@
 #include "mmu.h"
 #include "timer.h"
+#include "memmap.h"
 
 BYTE GB_mem_read(GB_gameboy_t *gb, int addr) {
     BYTE res = gb->memory[addr];
 
     if (addr <= 0x3FFF) {           /* Bank 00 */
-        res = gb->rom[addr];
+        if (addr < 0x100 && !GB_BOOT_ROM_UNMAPPED) {
+            res = BOOTROM[addr];
+        } else {
+            res = gb->rom[addr];
+        }
     } else if (addr <= 0x7FFF) {    /* Bank 01-NN */
         res = gb->rom[addr];
     }
 
 #ifdef ENABLE_GAMEBOY_DOCTOR_SETUP
-    else if (addr == 0xFF44) {
+    else if (addr == GB_LY_ADDR) {
         res = 0x90;
     }
 #endif
@@ -20,8 +25,10 @@ BYTE GB_mem_read(GB_gameboy_t *gb, int addr) {
 }
 
 void GB_mem_write(GB_gameboy_t *gb, int addr, BYTE data) {
-    if (addr == 0xFF44) { // LY
+    if (addr == GB_LY_ADDR) { // LY
         return;
+    } else if (addr == 0xFF46) {
+        printf("DMA\n");
     }
 
     TIMER_CHECK_WRITE(gb, addr, data);
