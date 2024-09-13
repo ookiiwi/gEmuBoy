b39edb97433291c01b3db730a89d5d302596275c
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-09-13 15:05:58 +0200

    Control execution flow frame by frame

--- include/gb.h
+++ include/gb.h
@@ -5,19 +5,24 @@
 #include "cpu.h"
 #include "ppu.h"
 
+typedef struct GB_clock_s GB_clock_t;
+
 struct GB_gameboy_s {
-    GB_cpu_t *cpu;
-    GB_ppu_t *ppu;
+    GB_cpu_t    *cpu;
+    GB_ppu_t    *ppu;
 
-    BYTE *memory;
-    BYTE *rom;
-    size_t              rom_size;
+    BYTE        *memory;
+    BYTE        *rom;
+    size_t      rom_size;
 
-    WORD                m_div;                      // DIV timer
+    GB_clock_t  *clock;
 };
 
 GB_gameboy_t*   GB_create(const char *src_rom_path);
 void            GB_destroy(GB_gameboy_t *gb);
 void            GB_run(GB_gameboy_t *gb);
+
+// DEBUG ONLY
+int GB_clock_avg_cycles(GB_gameboy_t *gb);
 
 #endif
--- src/gb.c
+++ src/gb.c
@@ -3,6 +3,37 @@
 #include "decode.h"
 #include "log.h"
 #include "timer.h"
+#include "memmap.h"
+#include "lcd.h"
+
+#include <SDL.h>
+#include <stdio.h>
+
+#define GB_MSEC_PER_FRAME   (1000/60)
+#define GB_CYCLES_PER_FRAME (4194304/60)
+
+struct GB_clock_s {
+    Uint64  last_tick;
+    Uint64  last_tick_cycle_cnt;
+    Uint64  avg_cycles;
+    int     avg_nb;    
+};
+
+GB_clock_t* GB_clock_create() {
+    GB_clock_t *clock = (GB_clock_t*)( malloc(sizeof(GB_clock_t)) );
+    if (clock == NULL) {
+        return NULL;
+    }
+
+    clock->last_tick            = 0;
+    clock->last_tick_cycle_cnt  = 0;
+
+    return clock;
+}
+
+void GB_clock_destroy(GB_clock_t *clk) {
+    if (clk) free(clk);
+}
 
 GB_gameboy_t* GB_create(const char *src_rom_path) {
     GB_gameboy_t *gb = (GB_gameboy_t*)( malloc( sizeof(GB_gameboy_t) ) );
@@ -10,6 +41,7 @@ GB_gameboy_t* GB_create(const char *src_rom_path) {
     gb->ppu     = NULL;
     gb->memory  = NULL;
     gb->rom     = NULL;
+    gb->clock   = NULL;
 
     if (gb == NULL) {
         return NULL;
@@ -22,6 +54,11 @@ GB_gameboy_t* GB_create(const char *src_rom_path) {
     gb->memory  = (BYTE*)( calloc( 0xFFFF, sizeof(BYTE) ) );
 
     if (gb->memory == NULL) {
+        return NULL;
+    }
+
+    gb->clock   = GB_clock_create();
+    if (gb->clock == NULL) {
         return NULL;
     }
 
@@ -53,6 +90,7 @@ void GB_destroy(GB_gameboy_t *gb) {
 
     if (gb == NULL) return;
 
+    GB_clock_destroy(gb->clock);
 #ifndef NO_PPU
     ppu_destroy(gb->ppu);
 #endif
@@ -64,21 +102,48 @@ void GB_destroy(GB_gameboy_t *gb) {
     free(gb);
 }
 
-void GB_run(GB_gameboy_t *gb) {
-    if (gb->cpu->m_is_halted) {
-        GB_handle_interrupt(gb);
-        GB_update_timer(gb);
-        return;
+// DEBUG ONLY
+int GB_clock_avg_cycles(GB_gameboy_t *gb) {
+    return gb->clock->avg_cycles/gb->clock->avg_nb;
+}
+
+int GB_clock_pulse(GB_gameboy_t *gb) {
+    Uint64 tick     = SDL_GetTicks64();
+    Uint64 delta    = tick - gb->clock->last_tick;
+    Uint64 cycles   = gb->cpu->m_cycle_counter - gb->clock->last_tick_cycle_cnt;
+
+    if (cycles < GB_CYCLES_PER_FRAME && delta < GB_MSEC_PER_FRAME) {
+        return 1;
     }
 
-    DECODE();
+    if (delta >= GB_MSEC_PER_FRAME) {
+        gb->clock->last_tick = tick;
+        gb->clock->last_tick_cycle_cnt = gb->cpu->m_cycle_counter;
 
-    // fetch
-    FETCH_CYCLE();
+        gb->clock->avg_cycles += cycles;
+        gb->clock->avg_nb++;
+    }
 
-    // ei_delay is assigned 2 by EI instr
-    // This allows IME to be set only after one instruction has passed
-    if (gb->cpu->m_ei_delay) {
-        _IME |= (gb->cpu->m_ei_delay--) & 1;
+    return 0;
+}
+
+void GB_run(GB_gameboy_t *gb) {
+    while (GB_clock_pulse(gb)) {
+        if (gb->cpu->m_is_halted) {
+            GB_handle_interrupt(gb);
+            INC_CYCLE();
+            return;
+        }
+
+        DECODE();
+
+        // fetch
+        FETCH_CYCLE();
+
+        // ei_delay is assigned 2 by EI instr
+        // This allows IME to be set only after one instruction has passed
+        if (gb->cpu->m_ei_delay) {
+            _IME |= (gb->cpu->m_ei_delay--) & 1;
+        }
     }
 }
