3809e2b899f48d6526d7999d1b6605d02382da80
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-09-25 15:29:09 +0200

    Remove old files and adapt main to new code base

--- include/defs.h
+++ include/defs.h
@@ -2,28 +2,17 @@
 #define DEFS_H_
 
 typedef struct GB_gameboy_s GB_gameboy_t;
-typedef struct GB_mmu_s GB_mmu_t;
 
-#define GET_MACRO(_0, _1, NAME, ...) NAME
-#define _INC_CYCLE0() do {                                              															\
-    gb->cpu->m_cycle_counter+=4;                                        															\
-	GB_dma_run(gb); 																												\
-    ppu_tick(gb);                                                       															\
-    GB_update_timer(gb);                                                															\
+#define INC_CYCLE() do {                                              															    \
+    gb->cpu->t_cycle_counter+=4;                                        															\
+    GB_timer_update(gb);                                               															    \
 } while(0)
-#define _INC_CYCLE1(step)   for (int i = 0; i < step; i++) _INC_CYCLE0()
-#define INC_CYCLE(...)      GET_MACRO(_0, ##__VA_ARGS__, _INC_CYCLE1, _INC_CYCLE0)(__VA_ARGS__)
 
 #define FETCH_CYCLE() do {                                                                                                          \
-    LOG_CPU_STATE();                                                                                                                \
-    GB_handle_interrupt(gb);                                                                                                        \
     PREV_IR = IR;                                                                                                                   \
     INC_CYCLE();                                                                                                                    \
     IR = GB_mem_read(gb, PC++);                                                                                                     \
-    if (gb->cpu->m_halt_bug) {  /* Either adjust RST's pushed address or else read byte twice */                                    \
-        PC--;                                                                                                                       \
-        gb->cpu->m_halt_bug = 0;                                                                                                    \
-    }                                                                                                                               \
+    GB_interrupt_handle(gb);                                                                                                        \
 } while(0)
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,41 +1,30 @@
 #include "gb.h"
-#include "instr.h"
-#include "decode.h"
+#include "cpudef.h"
+#include "cpu/decode.h"
 
-#define DUMMY_UPDATE_TIMER() do {} while(0)
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+    GB_gameboy_t *gb;
+
     if (argc < 2) {
         fprintf(stderr, "FORMAT ERROR: gEmuBoy <SRC_ROM_PATH>");
         exit(EXIT_FAILURE);
     }
 
-    GB_gameboy_t *gb;
-    const char *src_filename   = argv[1];
-
-    gb = GB_create(src_filename);    
-
-    if (gb == NULL) {
-        fprintf(stderr, "ERROR\n");
-        exit(EXIT_FAILURE);
+    gb = GB_gameboy_create(argv[1]);
+    if (!gb) {
+        fprintf(stderr, "CANNOT CREATE GAMEBOY\n");
+        return EXIT_FAILURE;
     }
 
-    // tmp init
     int i = 0;
-    PC = 0x00;  // TMP workaround test ppu
-    GB_mem_write(gb, 0xFF41, ( gb->memory[0xFF41] & 0xFC ) | (2 & 3) ); // TMP workaround to set PPU MODE 2
-    while(i++ < 0x10000) {
-        DECODE();
-
-        // fetch
-        FETCH_CYCLE();
-
-after_fetch_cycle:
-        DUMMY_UPDATE_TIMER();
-        /* TODO: UPDATE_TIMERS(); */
+    while(i++ < 0x900000) {
+        GB_cpu_run(gb);
     }
 
-    GB_destroy(gb);
+    GB_gameboy_destroy(gb);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
