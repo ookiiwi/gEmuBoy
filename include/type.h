5fdbd7b4a2adfd7238471936315a8369ca29a4f3
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-09-25 15:07:02 +0200

    Rename and use uint*_t types in typedef

---
+++ include/type.h
@@ -0,0 +1,12 @@
+#ifndef GB_TYPE_H_
+#define GB_TYPE_H_
+
+#include <stdint.h>
+
+typedef uint8_t     BYTE;
+typedef int8_t      SIGNED_BYTE;
+typedef uint16_t    WORD;
+typedef int16_t     SIGNED_WORD;
+typedef uint32_t    DWORD;
+
+#endif
