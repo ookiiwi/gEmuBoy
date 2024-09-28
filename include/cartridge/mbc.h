d4932f818032dad55c03c6f0c22ef9b3002b5524
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-09-28 18:57:34 +0200

    Impl mbc1 (only tested with cpu_instr.gb)

--- include/cartridge/mbc.h
+++ include/cartridge/mbc.h
@@ -5,10 +5,20 @@
 #include "defs.h"
 #include "cartridge/cartridge.h"
 
+struct GB_MBC_s {
+    BYTE bank_number;
+    int ram_enabled;
+    int banking_mode;
+};
+
+GB_MBC_t*  GB_MBC_create();
+void       GB_MBC_destroy(GB_MBC_t *mbc);
+
 #define MBC_DECL(nb)                                                            \
     BYTE GB_mbc##nb##_read(GB_cartridge_t *cartridge, WORD addr);               \
     void GB_mbc##nb##_write(GB_cartridge_t *cartridge, WORD addr, BYTE data);
 
 MBC_DECL(0);
+MBC_DECL(1);
 
 #endif
--- src/cartridge/mbc.c
+++ src/cartridge/mbc.c
@@ -1,10 +1,88 @@
 #include "cartridge/mbc.h"
 #include "cartridge/cartridge.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BANK_MASK       ( 1 << cartridge->header->rom_size )
+#define BANK1_NUMBER    ( ( cartridge->mbc->bank_number & 0x1F ) & BANK_MASK )
+#define BANK2_NUMBER    ( ( cartridge->mbc->bank_number & 0x60 ) & BANK_MASK )
+#define BANK_NUMBER     ( cartridge->mbc->bank_number  )
+#define RAMBANK_NUMBER  ( BANK2_NUMBER >> 5  )
+
+#define RAM_ENABLED     ( cartridge->mbc->ram_enabled )
+#define BANKING_MODE    ( cartridge->mbc->banking_mode )
+
+GB_MBC_t* GB_MBC_create() {
+    GB_MBC_t *mbc = (GB_MBC_t*)( malloc( sizeof (GB_MBC_t) ) );
+
+    if (mbc) {
+        mbc->bank_number    = 1;
+        mbc->banking_mode   = 0;
+    }
+
+    return mbc;
+}
+
+void GB_MBC_destroy(GB_MBC_t *mbc) {
+    if (mbc) free(mbc);
+}
+
 BYTE GB_mbc0_read(GB_cartridge_t *cartridge, WORD addr) {
     return cartridge->data[addr];
 }
 
 void GB_mbc0_write(GB_cartridge_t *cartridge, WORD addr, BYTE data) {
     cartridge->data[addr] = data;
+}
+
+BYTE GB_mbc1_read(GB_cartridge_t *cartridge, WORD addr) {
+    if (addr < 0x4000) {                                                // ROM Bank 00
+        if (BANKING_MODE) { // MODE1 uses Bank2 only
+            addr = (BANK2_NUMBER << 14) | (addr & 0x3FFF);
+        }
+
+        if (addr > ROM_SIZE(cartridge->header)) {
+            fprintf(stderr, "MEMORY OUTBOUND\n");
+            return 0xFF;
+        }
+
+        return cartridge->data[addr];
+    } else if (addr < 0x8000) {                                         // ROM Bank 01-7F
+        addr = (BANK_NUMBER << 14) | ( addr & 0x3FFF );
+
+        if (addr > ROM_SIZE(cartridge->header)) {
+            fprintf(stderr, "MEMORY OUTBOUND\n");
+            return 0xFF;
+        }
+
+        return cartridge->data[addr];
+    } else if (addr > 0x9FFF && addr < 0xC000) {         // RAM Bank 00-03
+        if (!RAM_ENABLED || !RAM_SIZE(cartridge->header)) {
+            fprintf(stderr, "NO RAM\n");
+            return 0xFF;
+        }
+        
+        if (BANKING_MODE && RAM_SIZE(cartridge->header) > 8*1024) {
+            addr = (BANK2_NUMBER << 8) | (addr & 0x1FFF);
+        }
+
+        return cartridge->data[addr];
+    }
+
+    return 0xFF;
+}
+
+void GB_mbc1_write(GB_cartridge_t *cartridge, WORD addr, BYTE data) {
+    if (addr < 0x1FFF) {                                                    // RAM Enable
+        cartridge->mbc->ram_enabled = data == 0x0A;
+    } else if (addr < 0x4000) {                                             // ROM Bank Number
+        data &= 0x1F;
+        cartridge->mbc->bank_number = BANK2_NUMBER | (data ? data : 1);
+    } else if (addr < 0x6000) {                                             // RAM Bank Number
+        data = (data &0x3) << 5;
+        cartridge->mbc->bank_number = data | BANK1_NUMBER;
+    } else if (addr < 0x8000) {
+        cartridge->mbc->banking_mode = (data != 0);
+    }
 }
