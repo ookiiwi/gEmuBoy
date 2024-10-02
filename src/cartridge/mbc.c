81498a5935c402d7a6a11e0bb4cf74cf866949cb
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-10-02 16:27:04 +0200

    Fix RAM address adjustement

--- src/cartridge/mbc.c
+++ src/cartridge/mbc.c
@@ -2,9 +2,16 @@
 #include "cartridge/cartridge.h"
 
 BYTE GB_mbc0_read(GB_cartridge_t *cartridge, WORD addr) {
+    if (addr > 0x9FFF && addr < 0xC000) {
+        addr -= 0x2000;
+    }
     return cartridge->data[addr];
 }
 
 void GB_mbc0_write(GB_cartridge_t *cartridge, WORD addr, BYTE data) {
-    cartridge->data[addr] = data;
+    if (addr < 0x8000) {
+        cartridge->data[addr] = data;
+    } else if (addr > 0x9FFF && addr < 0xC000) {
+        cartridge->data[addr-0x2000] = data;
+    }
 }
