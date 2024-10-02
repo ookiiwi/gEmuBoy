6755d4b9c17358422ed43be637e75f72049fbb31
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-10-02 16:22:48 +0200

    Check accessed address before adjusting and add handling for every address space

--- src/mmu.c
+++ src/mmu.c
@@ -15,44 +15,53 @@
 #define HRAM_START_ADDR     (0xFF80)
 #define IE_START_ADDR       (0xFFFF)
 
+#define ADJUST_ADDR(min, max) do {                                                          \
+    if ( addr < min || addr >= max) {                                                       \
+        fprintf(stderr, "OUT OF RANGE $%04X NOT IN RANGE [$%04X-$%04X]\n", addr, min, max); \
+        addr = max-1;                                                                       \
+    }                                                                                       \
+    addr -= min;                                                                            \
+} while(0)
+
 BYTE GB_mem_read(GB_gameboy_t *gb, WORD addr) {
-    if (addr < VRAM_START_ADDR) { // ROM bank
+    if (addr < VRAM_START_ADDR) {           // ROM bank     -- 0000-7FFF
         return gb->cartridge->read_callback(gb->cartridge, addr);
     } 
     
-    else if (addr < EXT_RAM_START_ADDR) {   // VRAM
+    else if (addr < EXT_RAM_START_ADDR) {   // VRAM         -- 8000-9FFF
         return GB_ppu_vram_read(gb->ppu, addr);
     } 
     
-    else if (addr < WRAM_START_ADDR) {      // RAM bank
-        return 0xFF;
+    else if (addr < WRAM_START_ADDR) {      // RAM bank     -- A000-BFFF
+        return gb->cartridge->read_callback(gb->cartridge, addr);
     } 
     
-    else if (addr < ECHO_RAM_START_ADDR) {  // WRAM
-        addr -= WRAM_START_ADDR;
+    else if (addr < ECHO_RAM_START_ADDR) {  // WRAM         -- C000-DFFF
+        ADJUST_ADDR(WRAM_START_ADDR, ECHO_RAM_START_ADDR);
         return gb->wram[addr];
     } 
     
-    else if (addr < OAM_START_ADDR) {       // Echo wram C000-DDFF
-        addr &= 0x1DFF; // TOODL
+    else if (addr < OAM_START_ADDR) {       // Echo wram    -- E000-FDFF
+        ADJUST_ADDR(ECHO_RAM_START_ADDR, OAM_START_ADDR);
         return gb->wram[addr];
     } 
     
-    else if (addr < UNUSABLE_START_ADDR) {  // OAM
-        return 0x00;
+    else if (addr < UNUSABLE_START_ADDR) {  // OAM          -- FE00-FE9F
+        return GB_ppu_oam_read(gb->ppu, addr);
     } 
     
-    else if (addr < IO_REGS_START_ADDR) {   // Not Usable cf. https://gbdev.io/pandocs/Memory_Map.html#fea0feff-range
-        return 0x00;
+    else if (addr < IO_REGS_START_ADDR) {   // Not Usable   -- FEA0-FEFF cf. https://gbdev.io/pandocs/Memory_Map.html#fea0feff-range
+        ADJUST_ADDR(UNUSABLE_START_ADDR, IO_REGS_START_ADDR);
+        return gb->unusable[addr];
     } 
     
-    else if (addr < HRAM_START_ADDR) {      // IO REGS
-        addr -= IO_REGS_START_ADDR;
+    else if (addr < HRAM_START_ADDR) {      // IO REGS      -- FF00-FF7F
+        ADJUST_ADDR(IO_REGS_START_ADDR, HRAM_START_ADDR);
         return gb->io_regs[addr];
     } 
     
-    else if (addr < IE_START_ADDR) {        // HRAM
-        addr -= HRAM_START_ADDR;
+    else if (addr < IE_START_ADDR) {        // HRAM         -- FF80-FFFE
+        ADJUST_ADDR(HRAM_START_ADDR, IE_START_ADDR);
         return gb->hram[addr];
     } 
 
@@ -74,36 +83,39 @@ void GB_mem_write(GB_gameboy_t *gb, WORD addr, BYTE data) {
     } 
     
     else if (addr < WRAM_START_ADDR) {      // RAM bank
+        gb->cartridge->write_callback(gb->cartridge, addr, data);
     }
     
     else if (addr < ECHO_RAM_START_ADDR) {  // WRAM
-        addr -= WRAM_START_ADDR;
+        ADJUST_ADDR(WRAM_START_ADDR, ECHO_RAM_START_ADDR);
         gb->wram[addr] = data;
     } 
     
     else if (addr < OAM_START_ADDR) {       // Echo wram C000-DDFF
-        addr -= 0xC000; // WRONG
+        ADJUST_ADDR(ECHO_RAM_START_ADDR, OAM_START_ADDR);
         gb->wram[addr] = data;
     } 
 
     else if (addr < UNUSABLE_START_ADDR) {  // OAM
-
+        GB_ppu_oam_write(gb->ppu, addr, data);
     }
     
     else if (addr < IO_REGS_START_ADDR) {   // Not Usable cf. https://gbdev.io/pandocs/Memory_Map.html#fea0feff-range
+        ADJUST_ADDR(UNUSABLE_START_ADDR, IO_REGS_START_ADDR);
+        gb->unusable[addr] = data;
     } 
     
     else if (addr < HRAM_START_ADDR) {      // IO REGS
-        addr -= IO_REGS_START_ADDR;
+        ADJUST_ADDR(IO_REGS_START_ADDR, HRAM_START_ADDR);
         gb->io_regs[addr] = data;
     } 
     
     else if (addr < IE_START_ADDR) {        // HRAM
-        addr -= HRAM_START_ADDR;
+        ADJUST_ADDR(HRAM_START_ADDR, IE_START_ADDR);
         gb->hram[addr] = data;
     } 
     
-    else { // IE
+    else {                                  // IE
         gb->ie = data;
     }
 }
