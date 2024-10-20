ef5f34a1165845982424fdc6b86f89f9e24a19db
Zakariya Hadrami <zakariyah.dev@gmail.com>
2024-10-20 21:20:43 +0200

    Pass bits_* and rom_* tests

--- src/cartridge/mbc.c
+++ src/cartridge/mbc.c
@@ -4,20 +4,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BANK_MASK       ( ( 2 << cartridge->header->rom_size ) - 1 )
-#define BANK1_NUMBER    ( ( cartridge->mbc->bank_number & 0x1F ) & BANK_MASK )
-#define BANK2_NUMBER    ( ( cartridge->mbc->bank_number & 0x60 ) & BANK_MASK )
-#define BANK_NUMBER     ( cartridge->mbc->bank_number & BANK_MASK )
-#define RAMBANK_NUMBER  ( BANK2_NUMBER >> 5  )
+#define ROM_BANK_MASK       ( ( 2 << cartridge->header->rom_size ) - 1 )
+#define ROM_BANK1_NUMBER    ( ( cartridge->mbc->bank_number & 0x1F ) & ROM_BANK_MASK )
+#define ROM_BANK2_NUMBER    ( ( cartridge->mbc->bank_number & 0x60 ) & ROM_BANK_MASK )
+#define ROM_BANK_NUMBER     ( ( cartridge->mbc->bank_number )        & ROM_BANK_MASK )
+#define RAM_BANK_NUMBER     ( ( cartridge->mbc->bank_number & 0x60 ) )
 
 #define RAM_ENABLED     ( cartridge->mbc->ram_enabled )
 #define BANKING_MODE    ( cartridge->mbc->banking_mode )
 
-#define CHECK_BOUNDERIES(a, b, msg, rv) do {                                                                \
-    if (phys_addr < a || phys_addr >= b) {                                                                  \
-        fprintf(stderr, "OUT OF RANGE %s BANK%d ($%04X => $%llX)\n", msg, BANK_NUMBER, addr, phys_addr);    \
-        return rv;                                                                                          \
-    }                                                                                                       \
+#define CHECK_BOUNDERIES(a, b, msg, rv) do {                                                                    \
+    if (phys_addr < a || phys_addr >= b) {                                                                      \
+        fprintf(stderr, "OUT OF RANGE %s BANK%d ($%04X => $%llX)\n", msg, ROM_BANK_NUMBER, addr, phys_addr);    \
+        return rv;                                                                                              \
+    }                                                                                                           \
 } while(0)
 
 GB_MBC_t* GB_MBC_create() {
@@ -26,6 +26,7 @@ GB_MBC_t* GB_MBC_create() {
     if (mbc) {
         mbc->bank_number    = 1;
         mbc->banking_mode   = 0;
+        mbc->ram_enabled    = 0;
     }
 
     return mbc;
@@ -62,7 +63,7 @@ BYTE GB_mbc1_read(GB_cartridge_t *cartridge, WORD addr) {
      * ┌────────────────────────────┬─────────────────────────────────────────┐
      * │                            │              ROM address bits           │
      * │     Accessed address       │    Bank number    │ Address within bank │
-     * ├────────────────────────────┼───────────────────┼─────────────────────┤
+     * ├────────────────────────────┼─────────┬─────────┼─────────────────────┤
      * │                            │  20─19  │  18─14  │       13─0          │
      * ├────────────────────────────┼─────────┼─────────┼─────────────────────┤
      * │ 0x0000─0x3FFF, MODE = 0b0  │  0b00   │ 0b00000 │     A<13:0>         │
@@ -70,12 +71,12 @@ BYTE GB_mbc1_read(GB_cartridge_t *cartridge, WORD addr) {
      * │ 0x0000─0x3FFF, MODE = 0b1  │  BANK2  │ 0b00000 │     A<13:0>         │
      * ├────────────────────────────┼─────────┼─────────┼─────────────────────┤
      * │        0x4000─0x7FFF       │  BANK2  │ BANK1   │     A<13:0>         │
-     * └────────────────────────────┴───────────────────┴─────────────────────┘
+     * └────────────────────────────┴─────────┴─────────┴─────────────────────┘
      */
 
     /**
      * ┌────────────────────────────┬─────────────────────────────────────────┐
-     * │                            │              ROM address bits           │
+     * │                            │              RAM address bits           │
      * │     Accessed address       │    Bank number    │ Address within bank │
      * ├────────────────────────────┼───────────────────┼─────────────────────┤
      * │                            │       14-13       │       12─0          │
@@ -90,27 +91,29 @@ BYTE GB_mbc1_read(GB_cartridge_t *cartridge, WORD addr) {
 
     if (addr < 0x4000) {                                                // ROM Bank 00
         if (BANKING_MODE) { // MODE1 uses Bank2 only
-            phys_addr = (BANK2_NUMBER << 14) | (addr & 0x3FFF);
+            phys_addr = (ROM_BANK2_NUMBER << 14) | (addr & 0x3FFF);
         }
 
         CHECK_BOUNDERIES(0, cartridge->data_size, "READ ROM", 0xFF);
 
         return cartridge->data[phys_addr];
     } else if (addr < 0x8000) {                                         // ROM Bank 01-7F
-        phys_addr = (BANK_NUMBER << 14) | ( addr & 0x3FFF );
+        phys_addr = (ROM_BANK_NUMBER << 14) | ( addr & 0x3FFF );
 
         CHECK_BOUNDERIES(0, cartridge->data_size, "READ ROM", 0xFF);
-        //printf("MBC1 READ (BANK%d) %04X => %llX\n", BANK_NUMBER, addr, phys_addr);
 
         return cartridge->data[phys_addr];
     } else if (addr > 0x9FFF && addr < 0xC000) {         // RAM Bank 00-03
         if (!RAM_ENABLED || !RAM_SIZE(cartridge->header)) {
-            fprintf(stderr, "NO RAM\n");
             return 0xFF;
         }
-        
+
+        phys_addr &= 0x1fff; // Use gameboy address bits 0-12
+
+        // TODO: pass ram tests
+
         if (BANKING_MODE && RAM_SIZE(cartridge->header) > 8*1024) {
-            phys_addr = (BANK2_NUMBER << 8) | (addr & 0x1FFF);
+            phys_addr |= (RAM_BANK_NUMBER << 8);
         }
 
         CHECK_BOUNDERIES(0, cartridge->data_size, "READ RAM BANK", 0xFF);
@@ -124,26 +127,27 @@ BYTE GB_mbc1_read(GB_cartridge_t *cartridge, WORD addr) {
 void GB_mbc1_write(GB_cartridge_t *cartridge, WORD addr, BYTE data) {
     uint64_t phys_addr = addr;
 
-    if (addr < 0x1FFF) {                                                    // RAM Enable
-        cartridge->mbc->ram_enabled = data == 0x0A;
+    if (addr < 0x2000) {                                                    // RAM Enable
+        cartridge->mbc->ram_enabled = ( (data&0xF) == 0xA );
     } else if (addr < 0x4000) {                                             // ROM Bank Number
         data &= 0x1F;
-        cartridge->mbc->bank_number = BANK2_NUMBER | (data ? data : 1);
-        //printf("BANK1 %X (%X)\n", BANK_NUMBER, data);
+        cartridge->mbc->bank_number = ROM_BANK2_NUMBER | (data ? data : 1);
     } else if (addr < 0x6000) {                                             // RAM Bank Number
         data = (data & 0x3) << 5;
-        cartridge->mbc->bank_number = data | BANK1_NUMBER;
+        cartridge->mbc->bank_number = data | ROM_BANK1_NUMBER;
     } else if (addr < 0x8000) {
-        cartridge->mbc->banking_mode = (data != 0);
+        cartridge->mbc->banking_mode = ((data&1) == 1);
     }
 
-    if (addr > 0x9FFF && addr < 0xC000) {         // RAM Bank 00-03
+    if (addr > 0x9FFF && addr < 0xC000) {                                   // RAM Bank 00-03
         if (!RAM_ENABLED || !RAM_SIZE(cartridge->header)) {
             return;
         }
+
+        phys_addr &= 0x1fff;
         
         if (BANKING_MODE && RAM_SIZE(cartridge->header) > 8*1024) {
-            phys_addr = (BANK2_NUMBER << 8) | (addr & 0x1FFF);
+            phys_addr |= (RAM_BANK_NUMBER << 8);
         }
 
         CHECK_BOUNDERIES(0, cartridge->data_size, "MBC1 WRITE RAM BANK", );
