b5eef2ac86d4037b229b0dca7a8ade16bd994694
Zakariya Hadrami <zakariyah.dev@gmail.com>
2025-02-16 12:43:27 +0100

    Add unused memory areas

--- src/mmu.c
+++ src/mmu.c
@@ -30,11 +30,11 @@ struct GB_mmu_s {
 	BYTE data_bus;
 };
 
-#define _START_ADDR(area_name_upper_case)   (GB_ ## area_name_upper_case ## _START_ADDR) 
+#define _BEG_ADDR(area_name_upper_case)   (GB_ ## area_name_upper_case ## _BEG_ADDR) 
 #define _END_ADDR(area_name_upper_case)     (GB_ ## area_name_upper_case ## _END_ADDR) 
 
 #define ADJUST_ADDR(area_name_upper_case) do {                                                                                                                      \
-    int min = _START_ADDR(area_name_upper_case);                                                                                                                    \
+    int min = _BEG_ADDR(area_name_upper_case);                                                                                                                    \
     int max = _END_ADDR(area_name_upper_case);                                                                                                                      \
     if ( addr <  min || addr > max ) {                                                                                                                              \
         fprintf(stderr, "OUT OF RANGE $%04X NOT IN RANGE [$%04X-$%04X]\n", addr, min, max);                                                                         \
@@ -43,7 +43,7 @@ struct GB_mmu_s {
     addr -= min;                                                                                                                                                    \
 } while(0)
 
-#define IF_ADDR_IN_RANGE(area_name_upper_case, statement)                                   if (addr >= _START_ADDR(area_name_upper_case) && addr <= _END_ADDR(area_name_upper_case)) { statement }
+#define IF_ADDR_IN_RANGE(area_name_upper_case, statement)                                   if (addr >= _BEG_ADDR(area_name_upper_case) && addr <= _END_ADDR(area_name_upper_case)) { statement }
 #define IF_ADDR(a, statement)                                                               if (addr == a) { statement }
 #define MEM_write_FUNC_DECL                                                                 void mem_write(GB_gameboy_t *gb, WORD addr, BYTE data)
 #define MEM_read_FUNC_DECL                                                                  BYTE mem_read(GB_gameboy_t *gb, WORD addr)
@@ -81,27 +81,31 @@ struct GB_mmu_s {
 
 #define DECL_MEM_ACCESSOR(access_type)                                                                                                                              \
     MEM_##access_type##_FUNC_DECL {                                                                                                                                 \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(ROM,                     GB_mbc,                 gb->cartridge->mbc)     /* ROM bank     -- 0000-7FFF */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(VRAM,                    GB_ppu_vram,            gb->ppu)                /* VRAM         -- 8000-9FFF */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(EXT_RAM,                 GB_mbc,                 gb->cartridge->mbc)     /* RAM bank     -- A000-BFFF */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (WRAM,                    wram)                                           /* WRAM         -- C000-DFFF */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(OAM,                     GB_ppu_oam,             gb->ppu)                /* OAM          -- FE00-FE9F */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (UNUSABLE,                unusable)                                       /* Not Usable   -- FEA0-FEFF */     \
-        /* ============= IO REGS ============= */                                                                                                                   \
-        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_JOYP_ADDR,            GB_joypad,              gb)                     /* Joypad       -- FF00      */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(SERIAL,                  _GB_io_reg,             gb->io_regs)            /* Serial       -- FF01-FF02 */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(TIMER_REGS,              GB_timer,               gb->io_regs)            /* Timer        -- FF04-FF07 */     \
-        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_IF_ADDR,              _GB_io_reg,             gb->io_regs)            /* Interrupts   -- FF0F      */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(APU,                     _GB_io_reg,             gb->io_regs)            /* Audio        -- FF10-FF26 */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(WAVE_PATTERN_RAM,        _GB_io_reg,             gb->io_regs)            /* Wave pattern -- FF30-FF3F */     \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(LCD_REGS,                GB_lcd,                 gb->io_regs)            /* LCD Control, -- FF40-FF4B */     \
-        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_BOOT_ROM_UNMAP_ADDR,  GB_boot_rom,            gb->io_regs)            /* Boot rom     -- FF50      */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(ROM,                         GB_mbc,                 gb->cartridge->mbc)     /* ROM bank     -- 0000-7FFF */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(VRAM,                        GB_ppu_vram,            gb)                     /* VRAM         -- 8000-9FFF */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(EXT_RAM,                     GB_mbc,                 gb->cartridge->mbc)     /* RAM bank     -- A000-BFFF */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (WRAM,                        wram)                                           /* WRAM         -- C000-DFFF */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(OAM,                         GB_ppu_oam,             gb->ppu)                /* OAM          -- FE00-FE9F */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (UNUSABLE,                    unusable)                                       /* Not Usable   -- FEA0-FEFF */     \
+        /* ============= IO REGS ============= */                                                                                                                       \
+        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_JOYP_ADDR,                GB_joypad,              gb)                     /* Joypad       -- FF00      */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(SERIAL,                      _GB_io_reg,             gb->io_regs)            /* Serial       -- FF01-FF02 */     \
+        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_SYSCLK_LSB_ADDR,          _GB_io_reg,             gb->io_regs)            /* Sysclk       -- FF03      */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(TIMER_REGS,                  GB_timer,               gb->io_regs)            /* Timer        -- FF04-FF07 */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(TIMER_INT_UNUSED_RANGE,      _GB_io_reg,             gb->io_regs)            /* Unused range -- FF08-FF0E */     \
+        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_IF_ADDR,                  _GB_io_reg,             gb->io_regs)            /* Interrupts   -- FF0F      */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(APU,                         _GB_io_reg,             gb->io_regs)            /* Audio        -- FF10-FF26 */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(APU_WAVEP_UNUSED_RANGE,      _GB_io_reg,             gb->io_regs)            /* Unused range -- FF27-FF29 */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(WAVE_PATTERN_RAM,            _GB_io_reg,             gb->io_regs)            /* Wave pattern -- FF30-FF3F */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(LCD_REGS,                    GB_lcd,                 gb->io_regs)            /* LCD Control, -- FF40-FF4B */     \
+        MAKE_MEM_##access_type##_ACCESS_CALLBACK      (GB_BOOT_ROM_UNMAP_ADDR,      GB_boot_rom,            gb->io_regs)            /* Boot rom     -- FF50      */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_CALLBACK(BOOTROM_HRAM_UNUSED_RANGE,   _GB_io_reg,             gb->io_regs)            /* Unused range -- FF51-FF7F */     \
         /* =================================== */                                                                                                                   \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (HRAM,                    hram)                                           /* HRAM         -- FF80-FFFE */     \
-        MAKE_MEM_##access_type##_ACCESS_VAR           (GB_IE_ADDR,              ie)                                             /* IE           -- FFFF      */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (HRAM,                        hram)                                           /* HRAM         -- FF80-FFFE */     \
+        MAKE_MEM_##access_type##_ACCESS_VAR           (GB_IE_ADDR,                  ie)                                             /* IE           -- FFFF      */     \
         /* Adjust address match wram address range in case echo ram is accessed */                                                                                  \
         addr -= 0x2000;                                                                                                                                             \
-        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (WRAM,                    wram)                                           /* Echo wram    -- E000-FDFF */     \
+        MAKE_MEM_##access_type##_RANGE_ACCESS_ARRAY   (WRAM,                        wram)                                           /* Echo wram    -- E000-FDFF */     \
         MEM_##access_type##_DEFAULT_RETURN;                                                                                                                         \
     }
 
@@ -112,7 +116,7 @@ BYTE GB_mem_read(GB_gameboy_t *gb, WORD addr) {
     if (gb->mmu->is_dma_active) {
 		// TODO: DMA conflicts
 
-		if (addr >= GB_OAM_START_ADDR && addr <= GB_OAM_END_ADDR) {
+		if (addr >= GB_OAM_BEG_ADDR && addr <= GB_OAM_END_ADDR) {
 			return 0xFF;
         }
 	}
@@ -122,7 +126,7 @@ BYTE GB_mem_read(GB_gameboy_t *gb, WORD addr) {
 
 void GB_mem_write(GB_gameboy_t *gb, WORD addr, BYTE data) {
     if (gb->mmu->is_dma_active) {
-		if (addr >= GB_OAM_START_ADDR && addr <= GB_OAM_END_ADDR) {
+		if (addr >= GB_OAM_BEG_ADDR && addr <= GB_OAM_END_ADDR) {
 			return;
         }
 	}
@@ -174,7 +178,7 @@ void GB_dma_run(GB_gameboy_t *gb) {
 			gb->mmu->dma_offset 	= 0;
 			// NOTE: break is omitted on purpose
     	case DMA_RUNNING: {
-			gb->mmu->addr_bus = GB_OAM_START_ADDR | gb->mmu->dma_offset;
+			gb->mmu->addr_bus = GB_OAM_BEG_ADDR | gb->mmu->dma_offset;
             gb->mmu->data_bus = mem_read(gb, (gb->mmu->dma_source | gb->mmu->dma_offset));
     		mem_write(gb, gb->mmu->addr_bus, gb->mmu->data_bus);
     		if (++gb->mmu->dma_offset > ( GB_OAM_END_ADDR & 0xFF ) ) { 	
